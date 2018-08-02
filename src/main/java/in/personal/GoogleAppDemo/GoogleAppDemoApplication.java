ad7c9ad02d02860c64be5f646b5296ce75ecc0a1
AnoopShiralige <anoop.shiralige@gmail.com>
2018-08-02 11:28:16 +1000

    Spring initializr project

---
+++ src/main/java/in/personal/GoogleAppDemo/GoogleAppDemoApplication.java
@@ -0,0 +1,12 @@
+package in.personal.GoogleAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAppDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/in/personal/GoogleAppDemo/GoogleAppDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package in.personal.GoogleAppDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleAppDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
